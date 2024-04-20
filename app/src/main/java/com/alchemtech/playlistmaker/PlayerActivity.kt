3999c8516a7b8a424c26834ad12dc5c824283a23
1lkham <offgenius@gmail.com>
2024-04-20 23:33:16 +0300

    13 спринт
    Player Designe

---
+++ app/src/main/java/com/alchemtech/playlistmaker/PlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.alchemtech.playlistmaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
