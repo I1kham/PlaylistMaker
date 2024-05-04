78e2dfbc710809fa2ba5f5f201ee47252d6f31fb
1lkham <offgenius@gmail.com>
2024-05-05 02:39:32 +0300

    14 спринт добавлен mediaPlayer

--- app/src/main/java/com/alchemtech/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/alchemtech/playlistmaker/PlayerActivity.kt
@@ -1,6 +1,9 @@
 package com.alchemtech.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.TypedValue
 import android.view.View
 import android.widget.Button
@@ -16,6 +19,23 @@ import java.util.Locale
 
 @Suppress("DEPRECATION")
 class PlayerActivity : AppCompatActivity() {
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+
+        private const val DEBOUNCE_GET_CURRENT_POSITION = 300L
+
+    }
+
+    private var playerState = STATE_DEFAULT
+
+    private var mediaPlayer = MediaPlayer()
+
+    var mainThreadHandler = Handler(Looper.getMainLooper())
+
+    private val currentPositionTask = createUpdateCurrentPositionTask()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,9 +59,108 @@ class PlayerActivity : AppCompatActivity() {
 
         playTimeFill(track)
 
-       albumCoverFill(track)
+        albumCoverFill(track)
+
+        preparePlayer()
+
+        playButWorking()
 
     }
+
+    private fun killCurrentPositionTask() {
+        mainThreadHandler.removeCallbacks(
+            currentPositionTask
+        )
+    }
+
+    private fun createUpdateCurrentPositionTask(): Runnable {
+        return object : Runnable {
+            override fun run() {
+
+                val currentPosition = SimpleDateFormat(
+                    "mm:ss",
+                    Locale.getDefault()
+                ).format(mediaPlayer.currentPosition)
+                val playTime = findViewById<TextView>(R.id.playTime)
+
+                playTime.text = currentPosition
+
+                mainThreadHandler.postDelayed(this, DEBOUNCE_GET_CURRENT_POSITION)
+
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun playButWorking() {
+        findViewById<ImageView>(R.id.playBut).setOnClickListener {
+            playbackControl()
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        val play = findViewById<ImageView>(R.id.playBut)
+        play.setImageResource(R.drawable.pause_but)
+        playerState = STATE_PLAYING
+        startGetCurrentPositionTask() // todo
+    }
+
+    private fun startGetCurrentPositionTask() {
+        mainThreadHandler.post(
+            currentPositionTask
+        )
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        val play = findViewById<ImageView>(R.id.playBut)
+        play.setImageResource(R.drawable.play_but)
+        playerState = STATE_PAUSED
+        killCurrentPositionTask()
+    }
+
+    private fun preparePlayer() {
+        val track = intent.getSerializableExtra("track") as Track
+        val play = findViewById<ImageView>(R.id.playBut)
+
+        mediaPlayer.setDataSource(track.previewUrl)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            play.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            killCurrentPositionTask()
+            val playTime = findViewById<TextView>(R.id.playTime)
+            playTime.text = "00:00"
+            play.setImageResource(R.drawable.play_but)
+            playerState = STATE_PREPARED
+
+        }
+    }
+
 
     private fun trackArtist(track: Track) {
         val trackArtist: TextView = findViewById(R.id.playerArtistName)
--- app/src/main/java/com/alchemtech/playlistmaker/track/Track.kt
+++ app/src/main/java/com/alchemtech/playlistmaker/track/Track.kt
@@ -8,10 +8,11 @@ data class Track(
     val trackTimeMillis: Long, // Продолжительность трека
     val artworkUrl100: String, // Ссылка на изображение обложки
     val trackId: String, //Id Track
-    val collectionName : String?, //название альбома
-    val releaseDate : String, // год релиза
-    val primaryGenreName : String, //жанр трека
-    val country : String // страна исполнителя
+    val collectionName: String?, //название альбома
+    val releaseDate: String, // год релиза
+    val primaryGenreName: String, //жанр трека
+    val country: String, // страна исполнителя
+    val previewUrl: String,
 ) : Serializable {
-    fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+    fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
 }
