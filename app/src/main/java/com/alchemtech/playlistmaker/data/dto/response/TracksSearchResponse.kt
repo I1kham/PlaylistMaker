728c5e5597e07d9c9d2850ec1a8c84d32a737401
1lkham <offgenius@gmail.com>
2024-07-03 03:00:58 +0300

    16 спринт переписал передачу трека через шаред

--- app/src/main/java/com/alchemtech/playlistmaker/data/dto/response/TracksSearchResponse.kt
+++ app/src/main/java/com/alchemtech/playlistmaker/data/dto/response/TracksSearchResponse.kt
@@ -3,7 +3,5 @@ package com.alchemtech.playlistmaker.data.dto.response
 import com.alchemtech.playlistmaker.data.dto.trackDto.TrackDto
 
 class TracksSearchResponse(
-    val searchType: String,
-    val expression: String,
     val results: List<TrackDto>,
 ) : Response()
--- app/src/main/java/com/alchemtech/playlistmaker/presentation/ui/TrackUtiils.kt
+++ app/src/main/java/com/alchemtech/playlistmaker/presentation/ui/TrackUtiils.kt
@@ -1,18 +1,16 @@
 package com.alchemtech.playlistmaker.presentation.ui
 
 import com.alchemtech.playlistmaker.domain.entity.Track
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 object TrackUtils {
 
     fun Track.getArtworkUrl512(): Any {
-        if (artworkUrl100!!.isNotEmpty()) {
-            return artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
+        return if (artworkUrl100!!.isNotEmpty()) {
+            artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
         } else {
-            return ""
+            ""
         }
     }
 
@@ -21,20 +19,8 @@ object TrackUtils {
     }
 
     fun Track.getReleaseDateString(): String {
-        if (releaseDate.isNullOrEmpty()) {
-            return ""
-        } else return releaseDate.substring(0 until 4) + " год"
-    }
-
-    fun Track.convertToString(): String {
-        return Gson().toJson(this)
-    }
-
-    fun convertFromString(string: String): Track {
-        return Gson().fromJson<Track>(
-            string,
-            object : TypeToken<Track?>
-                () {}.type
-        ) as Track
+        return if (releaseDate.isNullOrEmpty()) {
+            ""
+        } else releaseDate.substring(0 until 4) + " год"
     }
 }
