2befcd9a06a1dbbdddbc94bd5bd431a0d4de9676
I1kham <offgenius@gmail.com>
2024-07-15 13:20:24 +0300

    19 SingleActivity Fragments
    After review

--- app/src/main/java/com/alchemtech/playlistmaker/presentation/ui/main/StartActivity.kt
+++ app/src/main/java/com/alchemtech/playlistmaker/presentation/ui/main/StartActivity.kt
@@ -1,6 +1,9 @@
 package com.alchemtech.playlistmaker.presentation.ui.main
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +16,10 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class StartActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStartBinding
-
+companion object{
+    private val TOKEN= Any()
+    private const val SEARCH_DEBOUNCE_DELAY = 50
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModel by viewModel<StartViewModel>()
@@ -28,13 +34,13 @@ class StartActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(navController)
         navController.navInflater
 
-        val backPress = onBackPressedDispatcher.addCallback(
+        fun backPress() = onBackPressedDispatcher.addCallback(
             this,
             object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    bottomNavigationView.visibility = View.VISIBLE
-                    // if you want onBackPressed() to be called as normal afterwards
                     if (isEnabled) {
+                        println("6666666666666666666666666666666")
+                        bottomNavigationView.visibility = View.VISIBLE
                         isEnabled = false
                         onBackPressed()
                     }
@@ -44,7 +50,7 @@ class StartActivity : AppCompatActivity() {
         navHostFragment.childFragmentManager.addFragmentOnAttachListener { fragmentManager, fragment ->
             if (fragment.toString().contains("Player")) {
                 bottomNavigationView.visibility = View.GONE
-                backPress
+                backPress()
             }
         }
     }
